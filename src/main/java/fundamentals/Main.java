16a67399200a7a962a01c06afa7ab31c8b868898
NaghiAndrea <naghi.andrea@gmail.com>
2021-01-23 13:27:26 +0200

    Task 1: circle perimeter in main method

--- src/main/java/fundamentals/Main.java
+++ src/main/java/fundamentals/Main.java
@@ -1,7 +1,23 @@
 package fundamentals;
 
+import java.util.Scanner;
+
+
 public class Main {
     public static void main (String[]args) {
         System.out.println("Hello world!");
+
+        //Write an application that will read diameter of a circle (variable of type float) and
+        //calculate perimeter of given circle.
+        //Firstly, assume π = 3.14. Later, use value of π from built-in Math class.
+        Scanner input = new Scanner(System.in);
+        System.out.print("Insert diameter / Introduceti diametrul cercului: ");
+        float diameter = input.nextFloat();
+      //  float pi = 3.14f;
+        float pi = (float) Math.PI;
+        System.out.println("The perimeter: " + (diameter * pi));
+
+
+
     }
 }
